6b7b17bbd7efc3a07028847a811333fa827a2d8d
Setya Seng <setoseng@gmail.com>
2024-01-21 14:41:22 -0800

    Install Sharp for Image Optimization. Added font fallback to false to prevent error build

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -3,10 +3,12 @@ import { Inter, Newsreader } from "next/font/google";
 export const inter = Inter({
   subsets: ["latin"],
   display: "swap",
+  adjustFontFallback: false,
 });
 
 export const newsreader = Newsreader({
   weight: "300",
   subsets: ["latin"],
   display: "swap",
+  adjustFontFallback: false,
 });
